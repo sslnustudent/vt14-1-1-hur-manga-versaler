feaa829e25d9bfaf2e0b033a1155371194e0ba8b
sslnustudent <ss222tb@student.lnu.se>
2014-01-24 09:47:56 +0100

    Godkänd Lab1_1

--- Lab1_1Versaler/Lab1_1Versaler/Default.aspx
+++ Lab1_1Versaler/Lab1_1Versaler/Default.aspx
@@ -5,7 +5,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
-    <link href="Style5.css" rel="stylesheet" />
+    <link href="~/Style5.css" rel="stylesheet" />
 </head>
 <body>
     <h1 id="lol">Hur många versaler?</h1>
@@ -15,7 +15,7 @@
         <asp:TextBox ID="VersalerTextBox" runat="server" TextMode="MultiLine"></asp:TextBox>
         <asp:Label ID="VersalerLabel" runat="server" Text=""></asp:Label>
         <asp:Button ID="CountButton" runat="server" Text="Bestäm antalet vasaler" OnClick="CountButton_Click" />
-        <asp:Button ID="ClearButton" runat="server" Text="Rensa" OnClick="ClearButton_Click" Visible="false"/>
+        <asp:Button ID="ClearButton" runat="server" Text="Rensa" Visible="false"/>
 
     </div>
     </form>
--- Lab1_1Versaler/Lab1_1Versaler/Default.aspx.cs
+++ Lab1_1Versaler/Lab1_1Versaler/Default.aspx.cs
@@ -9,26 +9,21 @@ namespace Lab1_1Versaler
 {
     public partial class Default : System.Web.UI.Page
     {
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void CountButton_Click(object sender, EventArgs e)
         {
+
+
             VersalerLabel.Text = "Texten innehåller " + Convert.ToString(Model.TextAnalyzer.GetNumberOfCapitals(VersalerTextBox.Text)) + " versaller";
             VersalerTextBox.Enabled = false;
             CountButton.Visible = false;
             ClearButton.Visible = true;
 
-        }
-        protected void ClearButton_Click(object sender, EventArgs e)
-        {
-                VersalerLabel.Text = "";
-                VersalerTextBox.Text = "";
-                VersalerTextBox.Enabled = true;
-                CountButton.Visible = true;
-                ClearButton.Visible = false;
         }
     }
 }
